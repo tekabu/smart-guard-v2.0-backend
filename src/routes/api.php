2078bb0af9bd14752f5967075696b8fb248433de
Nino <nino@email.com>
2025-11-29 18:56:07 +0800

    refactor: Remove unused /user endpoint from api routes

--- src/routes/api.php
+++ src/routes/api.php
@@ -14,10 +14,6 @@ use App\Http\Controllers\Api\DeviceBoardController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get("/user", function (Request $request) {
-    return $request->user();
-})->middleware("auth:sanctum");
-
 // Users API
 Route::apiResource("users", UserController::class);
 
