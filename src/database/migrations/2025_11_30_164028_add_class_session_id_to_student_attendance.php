645620fb5daa85b98b1d96174d2eccb4151c775b
Nino <nino@email.com>
2025-11-30 16:43:30 +0800

    feat: Add class_session_id to student_attendance table

---
+++ src/database/migrations/2025_11_30_164028_add_class_session_id_to_student_attendance.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('student_attendance', function (Blueprint $table) {
+            $table->foreignId('class_session_id')
+                ->constrained('class_sessions')
+                ->cascadeOnDelete()
+                ->after('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('student_attendance', function (Blueprint $table) {
+            $table->dropConstrainedForeignId('class_session_id');
+        });
+    }
+};
